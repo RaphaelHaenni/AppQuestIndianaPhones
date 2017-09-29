af2a360799804e6eae6a464e175d49ecd1c143ec
Simonch8 <moeckli.soeckli@gmail.com>
2017-09-29 19:59:22 +0200

    App2 new project with structure generated

---
+++ App2/AppQuest2_Dechiffrierer/app/src/main/java/ch/appquest/indiana_phones/appquest2_dechiffrierer/TakePicture.java
@@ -0,0 +1,13 @@
+package ch.appquest.indiana_phones.appquest2_dechiffrierer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TakePicture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_picture);
+    }
+}
