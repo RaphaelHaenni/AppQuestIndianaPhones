025913c7cbc6baf69a578cb7c7c3a8502f8e11df
Simonch8 <moeckli.soeckli@gmail.com>
2017-10-13 14:00:43 +0200

    Take Picture Function added and Main Activity and Manifest modified

--- App2/AppQuest2_Dechiffrierer/app/src/main/java/ch/appquest/indiana_phones/appquest2_dechiffrierer/MainActivity.java
+++ App2/AppQuest2_Dechiffrierer/app/src/main/java/ch/appquest/indiana_phones/appquest2_dechiffrierer/MainActivity.java
@@ -1,25 +1,15 @@
 package ch.appquest.indiana_phones.appquest2_dechiffrierer;
 
-import android.content.DialogInterface;
+import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.v7.app.AlertDialog;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.InputType;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String lwort = "";
     private Button tpicture;
     private Button epicture;
 
@@ -43,75 +33,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, EditPicture.class));
             }
         });
-    }
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuItem menuItem = menu.add("Log");
-        menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                write_log();
-                return false;
-            }
-        });
-
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    private void write_log()
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Lösungswort");
-
-        // Set up the input
-        final EditText input = new EditText(this);
-        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-        input.setInputType(InputType.TYPE_CLASS_TEXT);
-        builder.setView(input);
-
-        // Set up the buttons
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                lwort = input.getText().toString();
-                Toast.makeText(getApplicationContext(), lwort, Toast.LENGTH_LONG).show();
-                try {
-                    log(lwort);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-
-        builder.show();
-
-
-    }
-
-    public void log(String wort) throws JSONException {
-        Intent intent = new Intent("ch.appquest.intent.LOG");
-
-        if (getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).isEmpty()) {
-            Toast.makeText(this, "Logbook App not Installed", Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        JSONObject json = new JSONObject();
-        json.put("task", "Dechiffrierer");
-        json.put("solution", wort);
-
-        // Achtung, je nach App wird etwas anderes eingetragen
-        String logmessage = json.toString();
-
-        intent.putExtra("ch.appquest.logmessage", logmessage);
-
-        startActivity(intent);
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                1);
     }
 }
--- App2/AppQuest2_Dechiffrierer/app/src/main/java/ch/appquest/indiana_phones/appquest2_dechiffrierer/TakePicture.java
+++ App2/AppQuest2_Dechiffrierer/app/src/main/java/ch/appquest/indiana_phones/appquest2_dechiffrierer/TakePicture.java
@@ -1,13 +1,97 @@
 package ch.appquest.indiana_phones.appquest2_dechiffrierer;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TakePicture extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Bitmap mImageBitmap;
+    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_picture);
+
+        if(Build.VERSION.SDK_INT>=24){
+            try{
+                Method m = StrictMode.class.getMethod("disableDeathOnFileUriExposure");
+                m.invoke(null);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.i("EXI", "IOException");
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file path
+        String root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath(); //Environment.getDataDirectory().getAbsolutePath();
+        File myDir = new File(root + "/appquest_images");
+        myDir.mkdirs();
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String fname = "Image-"+ timeStamp +".PNG";
+        File image = new File (myDir, fname);
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    private void scanMedia(String path) {
+        File file = new File(path);
+        Uri uri = Uri.fromFile(file);
+        Intent scanFileIntent = new Intent(
+                Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri);
+        sendBroadcast(scanFileIntent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            try {
+                mImageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.parse(mCurrentPhotoPath));
+                scanMedia(Uri.parse(mCurrentPhotoPath).getPath());
+                kill_activity();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    void kill_activity()
+    {
+        finish();
     }
 }
