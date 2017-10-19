cd092b829d208f7f22fbd0a352525457863d9ac4
Simon Chiarot <simon.chiarot@bict.ch>
2017-10-19 11:59:01 +0200

    ImageView created

--- App3/AppQuest3_Memory/app/src/main/java/ch/appquest/indiana_phones/appquest3_memory/MainActivity.java
+++ App3/AppQuest3_Memory/app/src/main/java/ch/appquest/indiana_phones/appquest3_memory/MainActivity.java
@@ -1,13 +1,28 @@
 package ch.appquest.indiana_phones.appquest3_memory;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView ib_1_1;
+    private ImageView ib_2_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ib_1_1 = (ImageView) findViewById(R.id.imageView1_1);
+        ib_1_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
