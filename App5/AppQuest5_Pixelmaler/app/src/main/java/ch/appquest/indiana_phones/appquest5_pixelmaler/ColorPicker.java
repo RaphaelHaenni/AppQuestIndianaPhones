8a2f8f5d051202d4fd3fdad63841aeb595fac845
Simon Chiarot <simon.chiarot@bict.ch>
2017-11-30 13:43:47 +0100

    I hate git

---
+++ App5/AppQuest5_Pixelmaler/app/src/main/java/ch/appquest/indiana_phones/appquest5_pixelmaler/ColorPicker.java
@@ -0,0 +1,17 @@
+package ch.appquest.indiana_phones.appquest5_pixelmaler;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ColorPicker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_picker);
+
+        //Todo
+        //MainActivity.color = new Color();
+    }
+}
---
+++ App5/AppQuest5_Pixelmaler/app/src/main/java/ch/appquest/indiana_phones/appquest5_pixelmaler/MainActivity.java
@@ -0,0 +1,16 @@
+package ch.appquest.indiana_phones.appquest5_pixelmaler;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static Color color;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
